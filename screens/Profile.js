abf61a3c9c6d51f9d7aec3fcc2e911f9bf19c570
MCucak <33839927+MCucak@users.noreply.github.com>
2020-05-24 21:44:17 +0300

    Added logout Button

--- screens/Profile.js
+++ screens/Profile.js
@@ -11,11 +11,28 @@ import {
   Button,
 } from 'react-native';
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
-
+import {AuthContext} from '../components/context'
 const ProfileScreen = ({navigation}) => {
+
+
+    const {signOut} = React.useContext(AuthContext);
+
   return (
     <View style={styles.container}>
+                <StatusBar 
+            backgroundColor= '#2E304F'
+            barStyle='light-content'
+        />
         <Text> BEN ProfileScreen HOŞGELDİN GARDAŞ</Text>
+        <Text> BEN ProfileScreen HOŞGELDİN GARDAŞ</Text>
+        <Text> BEN ProfileScreen HOŞGELDİN GARDAŞ</Text>
+        <View style={styles.butonExitView}>
+        <TouchableOpacity
+            style= {styles.touchableExitButton}
+            onPress={() => {signOut()}}>
+            <Text style={styles.exitButtonText}>Çıkış Yap</Text>
+            </TouchableOpacity>
+        </View>
     </View>
   );
   };
@@ -25,86 +42,31 @@ const ProfileScreen = ({navigation}) => {
   const styles = StyleSheet.create({
     container: {
       flex: 1, 
-      backgroundColor: '#009387'
+      backgroundColor: '#fff',
+      
     },
-    header: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        paddingHorizontal: 20,
-        paddingBottom: 50
-    },
-    footer: {
-        flex: 3,
-        backgroundColor: '#fff',
-        borderTopRightRadius: 50,
-        paddingHorizontal: 20,
-        paddingVertical: 30,
-        width:'90%',
-    },
-    text_header: {
-        color: '#fff',
-        fontWeight: 'bold',
-        fontSize: 30
-    },
-    text_footer: {
-        color: '#05375a',
-        fontSize: 18
-    },
-    action: {
-        flexDirection: 'row',
-        marginTop: 10,
-        borderBottomWidth: 2,
-        borderBottomColor: '#A9A9A9',
-        paddingBottom: 5,
-    },
-    actionError: {
-        flexDirection: 'row',
-        marginTop: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#FF0000',
-        paddingBottom: 5
-    },
-    textInput: {
-        flex: 1,
-        marginTop: Platform.OS === 'ios' ? 0 : -12,
-        paddingLeft: 5,
-        color: '#05375a',
-        paddingBottom:-5,
-    },
-    errorMsg: {
-        color: '#FF0000',
-        fontSize: 14,
-    },
-    button: {
-        alignItems: 'center',
-        marginTop: 50,
-        width:'100%',
-    },
-    signIn: {
-        width: '100%',
-        height: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 10
-    },
-    textSign: {
-        fontSize: 18,
-        fontWeight: 'bold'
-    },
-    touchableButton:{
-        marginTop:20,
+    touchableExitButton:{
+        marginTop:10,
         width:300,
         paddingTop:8,
         paddingBottom:8,
-        borderWidth: 1,
-        borderColor:'#BABABA',
+        borderWidth: 2,
+        borderColor:'#FF6633',
         borderRadius:8,
         alignItems:"center",
-        backgroundColor:'#9999ff',
+        backgroundColor:'#FF6633',
     },
-    touchableButtonText: {
+    butonExitView:{
+        marginBottom:10,
+        flex: 1,
+        justifyContent: 'flex-end',
+        marginBottom: 36,
+        alignItems:'center'
+    },
+    exitButtonText:{
+
         color:'#fff',
         fontSize:20,
-        fontWeight:"bold",
-    }
+        fontWeight:"normal",
+    },
   });
